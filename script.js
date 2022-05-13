892cf7f23225e604d2a202af8a79fc4bf8588feb
RuiShengGit <seow.ruisheng@gmail.com>
2022-05-13 12:14:53 +0800

    Added images to round display and colour changes

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const message = document.querySelector('.round-message');
 const scorePlayer = document.querySelector('.player-score');
 const scoreComputer = document.querySelector('.comp-score');
-const selectionPlayer =document.querySelector('.player_info');
+const selectionPlayer =doScument.querySelector('.player_info');
 const selectionComputer =document.querySelector('.pc_info');
 const images = Array.from(document.querySelectorAll('.option-img'));
 const playerDisplay =document.querySelector('.player_info');
 const pcDisplay =document.querySelector('.pc_info');
 
+/*
 let rockImg = document.createElement("img");
 rockImg.src = 'rock.png';
 rockImg.setAttribute("height", "148");
@@ -21,6 +22,7 @@ let scisImg = document.createElement("img");
 scisImg.src = 'scissors.png';
 scisImg.setAttribute("height", "148");
 scisImg.setAttribute("width", "148");
+*/
 
 var userScore = 0;
 var computerScore = 0;
@@ -44,23 +46,55 @@ function lose(){
 function draw(){
     message.textContent = "DRAAWWWWWW";
 }
+
+
 function playRound(playerSelection, computerSelection){
     switch(playerSelection + computerSelection){
     case "RS":
+        playerDisplay.innerHTML="<img src = rock.png height = 148 width = 148>";
+        pcDisplay.innerHTML="<img src = scissors.png height = 148 width = 148>"
+        win();
+        break;
     case "PR":
+        playerDisplay.innerHTML= "<img src = paper.png height = 148 width = 148>";
+        pcDisplay.innerHTML= "<img src = rock.png height = 148 width = 148>";
+        win();
+        break;
     case "SP":
+        playerDisplay.innerHTML= "<img src = scissors.png height = 148 width = 148>";
+        pcDisplay.innerHTML= "<img src = paper.png height = 148 width = 148>";
         win();
         break;
 
     case "RP":
+        playerDisplay.innerHTML="<img src = rock.png height = 148 width = 148>";
+        pcDisplay.innerHTML="<img src = paper.png height = 148 width = 148>"
+        lose();
+        break;
     case "PS":
+        playerDisplay.innerHTML="<img src = paper.png height = 148 width = 148>";
+        pcDisplay.innerHTML="<img src = scissors.png height = 148 width = 148>"
+        lose();
+        break;
     case "SR":
+        playerDisplay.innerHTML="<img src = scissors.png height = 148 width = 148>";
+        pcDisplay.innerHTML="<img src = rock.png height = 148 width = 148>";
         lose();
         break;
 
     case "RR":
+        playerDisplay.innerHTML="<img src = rock.png height = 148 width = 148>";
+        pcDisplay.innerHTML="<img src = rock.png height = 148 width = 148>"
+        draw();
+        break;
     case "PP":
+        playerDisplay.innerHTML="<img src = paper.png height = 148 width = 148>";
+        pcDisplay.innerHTML="<img src = paper.png height = 148 width = 148>"
+        draw();
+        break;
     case "SS":
+        playerDisplay.innerHTML="<img src = scissors.png height = 148 width = 148>";
+        pcDisplay.innerHTML="<img src = scissors.png height = 148 width = 148>"
         draw();
         break;
     }
@@ -72,6 +106,7 @@ images.forEach((image) =>
     if (userScore >= 5 || computerScore >= 5) {
       return;
     }
+    playerDisplay.remove
     game(image.dataset.image);
   })
 );
