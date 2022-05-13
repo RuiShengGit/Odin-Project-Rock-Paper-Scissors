3d40f669def81dc468f0b7ff2982516f5384ed76
RuiShengGit <seow.ruisheng@gmail.com>
2022-05-13 13:47:58 +0800

    Fixed minor error

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const message = document.querySelector('.round-message');
 const scorePlayer = document.querySelector('.player-score');
 const scoreComputer = document.querySelector('.comp-score');
-const selectionPlayer =doccument.querySelector('.player_info');
+const selectionPlayer =document.querySelector('.player_info');
 const selectionComputer =document.querySelector('.pc_info');
 const images = Array.from(document.querySelectorAll('.option-img'));
 const playerDisplay =document.querySelector('.player_info');
